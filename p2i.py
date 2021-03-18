96cb7616f4b0bfbf555cd2db1a359beecaec7957
Aamir Siddiqui <aamir.k306@yahoo.com>
2021-03-19 00:36:05 +0530

    croping box images (#2)
    * croping box images
    
    * croping box images
    
    * croping box images
    
    * extracting text
    
    * extracting text
    
    * extracting text
    
    * adding function
    
    * adding function

--- p2i.py
+++ p2i.py
@@ -1,10 +1,12 @@
 from typing import Optional
 from pdf2image import convert_from_path
 import os
-import pytesseract
+from tesseract2dict import TessToDict
 from PIL import Image
+import cv2
+import numpy as np
 
-TESSDATA_DIR_CONFIG = '--tessdata-dir "."'
+td=TessToDict()
 
 
 def pdf_to_image(
@@ -20,10 +22,33 @@ def pdf_to_image(
     return True
 
 
-def image_to_text(filename: str):
-    text = pytesseract.image_to_string(Image.open(filename), config=TESSDATA_DIR_CONFIG)
-    return text
 
+def image_to_text(pic_name: str)):
+    img = cv2.imread(pic_name)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+
+    kernel = np.ones((5,5),np.uint8)
+    erosion = cv2.erode(gray,kernel,iterations = 2)
+    kernel = np.ones((4,4),np.uint8)
+    dilation = cv2.dilate(erosion,kernel,iterations = 2)
+    edged = cv2.Canny(dilation, 30, 200)
+    cnt, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for i in range(len(cnt)):
+
+        area = cv2.contourArea(cnt[i])
+        if area>36000 and area<100000:
+#             print(i, area)
+            x,y,w,h = cv2.boundingRect(cnt[i])
+            crop= img[ y:h+y,x:w+x]
+    #         cv2.imwrite('cropped' + str(i) + '_'+ '.jpg', crop)
+    #         plt.figure(figsize= (5,5))
+    #         plt.imshow(crop)
+    #         plt.show()
+            word_dict=td.tess2dict(crop,'out','outfolder')
+            text_plain=td.word2text(word_dict,(0,0,crop.shape[1],crop.shape[0]))
+            #to do - save text_plain in pandas df
+            return text_plain
 
 pdf_to_image()
-print(image_to_text("./hat_1/page_15.jpg"))
+image_to_text('./hat_1/page_15.jpg')
